18f2f766b6f7d58dc629b661530860cd332af472
leovaldez08 <116164199+leovaldez08@users.noreply.github.com>
2022-11-22 17:56:00 +0530

    3 files modified

--- script.js
+++ script.js
@@ -1,28 +1,35 @@
 /* Iteration 4: Make the Everything Work */
-const buttonbackround =document.getElementById("button-ground");
-const backround =document.getElementById("ground");
+const buttonbackround = document.getElementById('button-ground');
+const backround = document.getElementById('ground');
 
 buttonbackround.oninput = () => {
-  backround.style.fill = buttonbackround.value
-}
+  backround.style.fill = buttonbackround.value;
+};
 
-const buttonbody =document.getElementById("button-body");
-const body =document.getElementById("body");
+const buttonbody = document.getElementById('button-body');
+const body = document.getElementById('body');
 
 buttonbody.oninput = () => {
-  body.style.fill = buttonbody.value
-}
+  body.style.fill = buttonbody.value;
+};
 
-const buttonears =document.getElementById("button-ears");
-const ears =document.getElementById("ears");
+const buttonears = document.getElementById('button-ears');
+const ears = document.getElementById('ears');
 
 buttonears.oninput = () => {
-  ears.style.fill = buttonears.value
-}
+  ears.style.fill = buttonears.value;
+};
 
-const buttoncheeks =document.getElementById("button-cheeks");
-const cheeks =document.getElementById("cheeks");
+const buttoncheeks = document.getElementById('button-cheeks');
+const cheeks = document.getElementById('cheeks');
 
 buttoncheeks.oninput = () => {
-  cheeks.style.fill = buttoncheeks.value
-}
+  cheeks.style.fill = buttoncheeks.value;
+};
+
+const buttonsky = document.getElementById('button-sky');
+const sky = document.getElementById('sky');
+
+buttonsky.oninput = () => {
+  sky.style.fill = buttonsky.value;
+};
